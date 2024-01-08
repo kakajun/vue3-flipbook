cdc660d2f129fd380505f3d1326e5ae88bd1d54d
马军 <majun@xiwicloud.com>
2024-01-08 21:09:24 +0800

    1

--- packages/storybook/src/stories/Flipbook.stories.js
+++ packages/storybook/src/stories/Flipbook.stories.js
@@ -1,5 +1,22 @@
 import Flipbook from '@/views/Flipbook.vue'
 
+const description = `
+The Phaser.Game instance is the main controller for the entire Phaser game.
+It is responsible for handling the boot process, parsing the configuration values, creating the renderer, and setting-up all of the global Phaser systems, such as sound and input.
+Once that is complete it will start the Scene Manager and then begin the main game loop.
+
+You should generally avoid accessing any of the systems created by Game, and instead use those made available to you via the Phaser.Scene Systems class instead.
+
+\`\`\`html
+<Game :config="gameConfig">
+  <Scene name="SceneName1" />
+  <Scene name="SceneName2" />
+</Game>
+\`\`\`
+
+See also: [Phaser.Game](https://newdocs.phaser.io/docs/Phaser.Game)
+`
+
 const getPages = async (isHi) => {
   const importAll = async (r) => {
     const images = []
@@ -17,14 +34,27 @@ const getPages = async (isHi) => {
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 export default {
   title: 'flipDuration',
+  parameters: {
+    docs: {
+      description: {
+        component: description
+      }
+    }
+  },
   component: Flipbook,
   tags: ['autodocs'],
+  args: {
+    zoomDuration: 2500
+  },
   argTypes: {
     flipDuration: {
+      name: ':flipDuration',
+      description:
+        'The configuration object for your Phaser Game instance.<br>See: [Phaser.Types.Core.GameConfig](https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig)',
       control: {
         type: 'select'
       },
-      options: [1000, 2000, 3000]
+      options: [1000, 3000, 5000]
     }
   }
 }
@@ -36,7 +66,12 @@ export const SlowOne = {
     setup() {
       return { args, pages, pagesHiRes }
     },
-    template: '<Flipbook class="flipbook" :pages="pages" :pagesHiRes="pagesHiRes" />'
+    template: `<Flipbook class="flipbook"
+       :pages="pages"
+       :flipDuration="args.flipDuration"
+       :pagesHiRes="pagesHiRes"
+       :zoomDuration="args.zoomDuration"
+       />`
   }),
   loaders: [
     async () => ({
