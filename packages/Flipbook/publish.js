f323cf0dc2546ff705ac6e8dbf73caff14aab32e
马军 <majun@xiwicloud.com>
2024-01-11 22:56:26 +0800

    fix: publish

--- packages/Flipbook/publish.js
+++ packages/Flipbook/publish.js
@@ -1,8 +1,13 @@
+const { createConsola } = require('consola')
+const logger = createConsola({
+  level: 4 // 设置日志级别为 silent
+})
+
 const fs = require('fs')
 const path = require('path')
 const { execSync } = require('child_process')
 
-const DIR_VARIABLE = path.join(process.cwd(), './packages/Flipbook')
+const DIR_VARIABLE = path.join(process.cwd(), './')
 const VERSION_TO_UPDATE = 'patch'
 
 const setVersionToJson = (version) => {
@@ -14,30 +19,25 @@ const setVersionToJson = (version) => {
 }
 
 const run = async () => {
-
   if (!process.env.NPM_AUTH_TOKEN) throw new Error('Merge-release requires NPM_AUTH_TOKEN')
-  // execSync('node ./postbuild.js')
-
   const pkg = require(path.join(DIR_VARIABLE, 'package.json'))
 
   const currentVersion = execSync(`npm view ${pkg.name} version`, {
     cwd: DIR_VARIABLE
   }).toString()
-  setVersionToJson(currentVersion)
+  logger.info('currentVersion: ', currentVersion)
+  // setVersionToJson(currentVersion)
 
   let newVersion = execSync(`npm version --no-git-tag-version ${VERSION_TO_UPDATE}`, {
     cwd: DIR_VARIABLE
   }).toString()
   newVersion = newVersion.replace(/(\r\n|\n|\r)/gm, '')
-
+  logger.info('newVersion:', newVersion)
   setVersionToJson(newVersion)
-
-  console.log('new version:', newVersion)
-
   execSync(`npm set //registry.npmjs.org/:_authToken=${process.env.NPM_AUTH_TOKEN}`)
   execSync(`npm publish --verbose ${DIR_VARIABLE}`)
-  execSync(`git checkout package.json`)
-
+  // execSync(`git checkout package.json`)
+  logger.success('publish success!')
   execSync(`git tag ${newVersion}`)
 
   execSync('git push --tags')
